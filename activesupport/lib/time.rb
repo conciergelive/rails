957eeccc48d976656faf2ebee5a07237eadb2ec4
Ben Johnson <bjohnson@binarylogic.com>
2014-04-30 18:32:11 -0400

    add time.rb file

---
+++ activesupport/lib/time.rb
@@ -0,0 +1,4 @@
+# here to suppress errors for require "active_support/time"
+# newer versions of rails support that path, this allows us
+# to support the clockwork gem without having to maintain
+# our own copy / fork
